956c85f24cfde269f2fed58d28d948d76df2caff
Anton <anton@Eee-PC.(none)>
2011-11-18 23:46:46 +0400

    empty proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NiftyNotes::Application.config.secret_token = '80b0086695a219a49ec591583c7c4c735c684224ce266cb5808dc52e9bb0d8ab6f47fe35f8ae9fb8d4627a9e2807348b28dce0a6692f91a86a9a23ce2b616975'
